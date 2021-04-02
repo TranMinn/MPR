d1b2745c35432540a966bfceb6c515d91ddeef72
TranMinn <1801040141@s.hanu.edu.vn>
2021-04-02 10:05:46 +0700

    commit

--- CustomNotification/app/src/main/java/com/example/customnotification/MainActivity.java
+++ CustomNotification/app/src/main/java/com/example/customnotification/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.customnotification;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import static com.example.customnotification.App.CHANNEL_ID;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+    }
+
+    public void showNotification(View v) {
+        RemoteViews collapsedView = new RemoteViews(getPackageName(),
+                R.layout.notification_collapsed);
+        RemoteViews expandedView = new RemoteViews(getPackageName(),
+                R.layout.notification_expanded);
+
+        Intent clickIntent = new Intent(this, NotificationReceiver.class);
+        PendingIntent clickPendingIntent = PendingIntent.getBroadcast(this,
+                0, clickIntent, 0);
+
+        collapsedView.setTextViewText(R.id.text_view_collapsed_1, "Hello World!");
+
+        expandedView.setImageViewResource(R.id.image_view_expanded, R.drawable.luffy_funny);
+        expandedView.setOnClickPendingIntent(R.id.image_view_expanded, clickPendingIntent);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.noti)
+                .setCustomContentView(collapsedView)
+                .setCustomBigContentView(expandedView)
+                //.setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                .build();
+
+        notificationManager.notify(1, notification);
     }
 }
