9f9ba194af3f2df3b528d0a3d1b086863ca24226
TranMinn <1801040141@s.hanu.edu.vn>
2021-04-07 22:14:14 +0700

    updatee

--- MyChannels/app/src/main/java/com/example/mychannels/MainActivity.java
+++ MyChannels/app/src/main/java/com/example/mychannels/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mychannels;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextTitle;
     private EditText editTextMessage;
 
+    private static Uri notiSound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +39,24 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
+        // Notification Sound
+        notiSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        // Notification Intent
+        Intent intent = new Intent(MainActivity.this, Intent1.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("message", message);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1)
                 .setSmallIcon(R.drawable.penguin)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setSound(notiSound)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(pendingIntent)
                 .build();
 
         notificationManager.notify(1, notification);
@@ -48,11 +66,24 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
+        // Notification Sound
+        notiSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        // Notification Intent
+        Intent intent = new Intent(MainActivity.this, Intent2.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("message", message);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_2)
                 .setSmallIcon(R.drawable.hamster)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setSound(notiSound)
                 .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setContentIntent(pendingIntent)
                 .build();
 
         notificationManager.notify(2, notification);
